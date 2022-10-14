69474f524a7c0e6e6b8472ff4d3e566b3940ede8
Michelle Hendriks <106346778+mcphendriks@users.noreply.github.com>
2022-10-14 11:14:44 +0200

    version 2 commited

---
+++ versie-2/script.js
@@ -0,0 +1,47 @@
+// DOM
+const swiper = document.querySelector('#swiper');
+const like = document.querySelector('#like');
+const dislike = document.querySelector('#dislike');
+
+// IMAGES
+const urls = [
+  'https://plnts.com/_next/image?url=https%3A%2F%2Fwebshop.plnts.com%2Fmedia%2Fcatalog%2Fproduct%2Fcache%2Faa5d334f459227518b6c3cf7ea9d29ed%2Fp%2Fl%2Fpl.xl.005-2.jpg&w=750&q=100',
+  'https://www.plantje.nl/wp-content/uploads/2019/01/Schefflera-op-lava-steen-plantje-1024-600x600.jpg.webp',
+  'https://www.plantje.nl/wp-content/uploads/2022/05/phlebodium-aureum-P17-1024-1-600x600.jpg.webp',
+  'https://www.plantje.nl/wp-content/uploads/2021/09/Aeschynanthus-Japhrolepis-Lifestyle-600x600.jpg.webp',
+  'https://www.plantje.nl/wp-content/uploads/2021/09/Spathiphyllum-Torelli-p12-1-600x600.jpg.webp',
+  'https://www.plantje.nl/wp-content/uploads/2022/10/Parthenocissus-Striata-Suikerdruifje-P12-1024-1-600x600.webp'
+];
+
+// VARIABLE TO TRACK THE COUNT OF CARDS
+let cardCount = 0;
+
+// FUNCTION TO APPEND A CARD
+function appendNewCard() {
+  const card = new Card({
+    imageUrl: urls[cardCount % urls.length], // MAKES THE INDEX NOT EXCEED THE COUNT OF IMAGES. ALSO NECESSARY FOR IMAGE LOOPING
+    onDismiss: appendNewCard,
+    onLike: () => {
+      like.style.animationPlayState = 'running';
+      like.classList.toggle('trigger');
+    },
+    onDislike: () => {
+      dislike.style.animationPlayState = 'running';
+      dislike.classList.toggle('trigger');
+    }
+  });
+  swiper.append(card.element);
+  cardCount++;
+
+  // SELECT ALL THE CARDS THAT ARE NOT DISMISSED
+  const cards = swiper.querySelectorAll('.card:not(.dismissing)');
+  cards.forEach((card, index) => {
+    // ADD A NEW PROPERTY RESPONSIBLE FOR THE TILT
+    card.style.setProperty('--i', index);
+  });
+}
+
+// APPEND THE CARDS
+for (let i = 0; i < urls.length; i++) {
+  appendNewCard();
+}
