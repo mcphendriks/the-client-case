9995ada5527c824e2448c4fe48740edbacbfc7e8
Michelle Hendriks <106346778+mcphendriks@users.noreply.github.com>
2022-11-11 13:41:47 +0100

    set position form1

--- Contact form.js
+++ Contact form.js
@@ -1,35 +1,28 @@
-let Form1 = document.getElementById ("Form1");
-let Form2 = document.getElementById ("Form2");
-let Form3 = document.getElementById ("Form3");
+// var Form1 = document.getElementById ("Form1");
+// var Form2 = document.getElementById ("Form2");
+// var Form3 = document.getElementById ("Form3");
 
-let Volgende1 = document.getElementById ("Volgende1");
-let Volgende2 = document.getElementById ("Volgende2");
-let Terug2 = document.getElementById ("Terug2");
-let Terug3 = document.getElementById ("Terug3");
+// var Volgende1 = document.getElementById ("Volgende1");
+// var Volgende2 = document.getElementById ("Volgende2");
+// var Terug2 = document.getElementById ("Terug2");
+// var Terug3 = document.getElementById ("Terug3");
 
-let Progres = document.getElementById ("Progres");
+// Volgende1.onclick = function () {
+//     Form2.style.left = "-450px";
+//     Form3.style.left = "40px";
+// }
 
-Volgende1.onclick = function () {
-    Form1.style.left = "-450px";
-    Form2.style.left = "40px";
-    Progres.style.width = "240px";
-}
+// Terug2.onclick = function () {
+//     Form1.style.left = "40px";
+//     Form2.style.left = "450px";
+// }
 
-Terug2.onclick = function () {
-    Form1.style.left = "40px";
-    Form2.style.left = "-450px";
-    Progres.style.width = "120px";
-}
+// Volgende2.onclick = function () {
+//     Form2.style.left = "-450px";
+//     Form3.style.left = "40px";
+// }
 
-Volgende2.onclick = function () {
-    Form2.style.left = "-450px";
-    Form3.style.left = "40px";
-    Progres.style.width = "360px";
-}
-
-Terug3.onclick = function () {
-    Form2.style.left = "40px";
-    Form3.style.left = "-450px";
-    Progres.style.width = "240px";
-}
-
+// Terug2.onclick = function () {
+//     Form2.style.left = "40px";
+//     Form3.style.left = "-450px";
+// }
